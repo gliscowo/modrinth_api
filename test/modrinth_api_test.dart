2fef2e0d8089d85814f2dd35f86829736961f922
glisco <commit.gpgsign>
2022-11-10 11:26:48 +0100

    implement 'latestFilesWithLoaderAndGameVersion'

--- test/modrinth_api_test.dart
+++ test/modrinth_api_test.dart
@@ -1,11 +1,23 @@
+import 'dart:io';
+
+import 'package:modrinth_api/src/modrinth_api_base.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
+  group('main', () {
+    ModrinthApi? modrinth;
     setUp(() {
-      // Additional setup goes here.
+      modrinth = ModrinthApi.createClient("gliscowo/modrinth-api/test-runner");
     });
 
-    test('First Test', () {});
+    test('latest files from hashes', () async {
+      print(
+          (await modrinth!.latestFilesWithLoaderAndGameVersion([File("deathlog-0.2.9+1.18.jar")], "fabric", "1.19.2"))!
+              .map((key, value) => MapEntry(key, value.versionNumber)));
+    });
+
+    test('search', () async {
+      print((await modrinth!.search("owo-lib")).hits.first.title);
+    });
   });
 }
