acf10b83c46378676a123d60f6d0a7ee40daa504
glisco <glisco03@web.de>
2023-08-03 13:41:25 +0200

    add Hash to public API

--- lib/modrinth_api.dart
+++ lib/modrinth_api.dart
@@ -8,3 +8,4 @@ export 'src/models/response_models.dart';
 export 'src/modrinth_api_base.dart' show ModrinthApi, RatelimitHandling;
 export 'src/result.dart';
 export 'src/routes/search.dart' show Facet, QueryString;
+export 'src/routes/version_files.dart' show Hash;
