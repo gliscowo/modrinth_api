8ebf26916bc181db0af3c773a8bf1b0dbf9ec1d0
glisco <glisco03@web.de>
2023-08-09 02:54:52 +0200

    expose exception types to public API

--- lib/modrinth_api.dart
+++ lib/modrinth_api.dart
@@ -3,6 +3,7 @@
 /// More dartdocs go here.
 library;
 
+export 'src/errors.dart';
 export 'src/models/request_models.dart';
 export 'src/models/response_models.dart';
 export 'src/modrinth_api_base.dart' show ModrinthApi, RatelimitHandling;
